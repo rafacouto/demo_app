80f1b9204f1f79d293fd4e8801d4e2fa7b97c3a3
Rafa Couto <rafacouto@gmail.com>
2012-08-05 19:55:58 +0200

    RAILS 3.2.7 skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8a1db593fb0d58dbbb5523dc9110a2c5dd3e5a98ab335c4747bf2ec67f2c5767c4e98c360499c06499d3bafdd6ec5883bcfb6e21674769e7f16a9655ed66de17'
